316fad2eb703f249771d312456723ce43b828ae1
Sean Caranzo <skybluenako@gmail.com>
2021-03-03 14:40:37 +0800

    whitespace changes

--- lab.py
+++ lab.py
@@ -10,6 +10,7 @@ def sequential(a_list, item):
     
     return found
 
+
 def ordered_sequential(a_list, item):
     pos = 0
     found = False
@@ -25,6 +26,7 @@ def ordered_sequential(a_list, item):
                 pos += 1
     
     return found
+
 
 def binary_search(a_list, item):
     first = 0
@@ -43,6 +45,7 @@ def binary_search(a_list, item):
                 
     return found
 
+
 def bubble_sort(a_list):
     for pairs in range(len(a_list)-1, 0, -1):
         for i in range(pairs):
@@ -50,7 +53,8 @@ def bubble_sort(a_list):
                 temp = a_list[i]
                 a_list[i] = a_list[i+1]
                 a_list[i+1] = temp
-                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+                                                                                                                                                                                                                                                                                                                                                                                                                          
 def short_bubble_sort(a_list):
     pairs = len(a_list) - 1
     exchange = True
@@ -65,20 +69,22 @@ def short_bubble_sort(a_list):
                 a_list[i+1] = temp
         
         pairs -= 1
-        
+
+
 def selection_sort(a_list):
     for i in range(len(a_list)):
         current_min = i
         
         for j in range(i+1, len(a_list)):
             if a_list[j] < a_list[current_min]:
-                current_min = j 
-    
+                current_min = j
+
         if current_min != i:
             temp = a_list[i]
             a_list[i] = a_list[current_min]
             a_list[current_min] = temp
-                                                                                                         
+
+                                                                                                                                         
 def insertion_sort(a_list):
     for i in range(1, len(a_list)):
         current = a_list[i]
@@ -87,9 +93,10 @@ def insertion_sort(a_list):
         while pos > 0 and a_list[pos-1] > current:
             a_list[pos] = a_list[pos-1]
             pos -= 1
-    
+
         a_list[pos] = current
-   
+
+
 def shell_sort(a_list):
     sublistcount = len(a_list)//2
     
@@ -97,10 +104,11 @@ def shell_sort(a_list):
         
         for startpos in range(sublistcount):
             gapInsertionSort(a_list, startpos, sublistcount)
-            
+        
         print('After increment of size', sublistcount, 'The list is', a_list)
         sublistcount//=2
-       
+
+
 def gapInsertionSort(a_list, start, gap):
     for i in range(start+gap, len(a_list), gap):
         current = a_list[i]
@@ -130,22 +138,23 @@ class Node():
     
     def set_next(self, item):
         self.next = item
-                                             
+
+                   
 class LinkedList():
     
     def __init__(self):
         self.head = None
-                                       
+
     # isEmpty(): tests to see whether the list is empty. It needs no parameters and returns a boolean value.                                    
     def is_empty(self):
         return self.head == None
-     
+
     # add(item): adds a new item to the list. It needs the item and returns nothing. Assume the item is not already in the list.
     def add(self, item):
         temp = Node(item)
         temp.set_next(self.head)
         self.head = temp
-                                                         
+
     # append(item): adds a new item to the end of the list making it the last item in the collection. It needs the item and returns nothing. Assume the item is not already in the list.             
     def append(self, item):
         current = self.head
@@ -157,7 +166,7 @@ class LinkedList():
             current.set_next(Node(item))
         else:
             self.head = Node(item)
-                                                                                          
+
     # insert(pos,item): adds a new item to the list at position pos. It needs the item and returns nothing. Assume the item is not already in the list and there are enough existing items to have position pos.      
     def insert(self, pos, item):
         count = 0
@@ -174,7 +183,7 @@ class LinkedList():
                 current = current.get_next()
             temp.set_next(current)
             previous.set_next(temp)
-                             
+
     # size(): returns the number of items in the list. It needs no parameters and returns an integer.                             
     def size(self):
         count = 0
@@ -185,7 +194,7 @@ class LinkedList():
             current = current.get_next()
         
         return count
-    
+
     # search(self, item): searches for the item in the list. It needs the item and returns a boolean value.                                                  
     def search(self, item):
         current = self.head
@@ -198,7 +207,7 @@ class LinkedList():
                 current = current.get_next()
         
         return found
-    
+
     # index(self, item): returns the position of item in the list. It needs the item and returns the index. Assume the item is in the list.
     def index(self, item):
         count = 0
@@ -214,7 +223,7 @@ class LinkedList():
                 current = current.get_next()
         
         return -1
-    
+
     # index type II          
     def index(self, item):
         count = 0
@@ -228,7 +237,7 @@ class LinkedList():
                 current = current.get_next()
         
         return -1             
-    
+
     # remove(self, item): removes the item from the list.                                                                        
     def remove(self, item):
         current = self.head
@@ -248,7 +257,7 @@ class LinkedList():
                 previous.set_next(current.get_next())
         else:
             pass
-                              
+
     # pop(self): removes and returns the last item in the list. It needs nothing and returns an item. Assume the list has at least one item.                                           
     def pop(self):
         current = self.head
@@ -263,7 +272,7 @@ class LinkedList():
             previous.set_next(None)
         
         return current.get_data()
-    
+
     # poppos(self, pos): removes and returns the item at position pos. It needs the position and returns the item. Assume the item is in the list.              
     def pospop(self, pos):
         count = 0
@@ -280,7 +289,7 @@ class LinkedList():
             previous.set_next(current.get_next())
         
         return current.get_data()
-    
+
 mylist = LinkedList()
 mylist.add(3)
 mylist.add(4)
