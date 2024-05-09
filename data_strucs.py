d818be29fb025111c3c83f54e3a4d7edd1577571
29yabuki <skybluenako@gmail.com>
2024-05-09 14:24:36 +0800

    changes in name and whitespaces

--- data_strucs.py
+++ data_strucs.py
@@ -1,36 +1,36 @@
 class Stack:
-     def __init__(self):
-         self.items = []
-
-     def isEmpty(self):
-         return self.items == []
-
-     def push(self, item):
-         self.items.append(item)
-
-     def pop(self):
-         return self.items.pop()
-
-     def peek(self):
-         return self.items[len(self.items)-1]
-
-     def size(self):
-         return len(self.items)
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+    
+    def size(self):
+        return len(self.items)
 
 
 class Queue:
     def __init__(self):
         self.items = []
-
-    def isEmpty(self):
+    
+    def is_empty(self):
         return self.items == []
-
+    
     def enqueue(self, item):
-        self.items.insert(0,item)
-
+        self.items.insert(0, item)
+    
     def dequeue(self):
         return self.items.pop()
-
+    
     def size(self):
         return len(self.items)
 
@@ -39,19 +39,19 @@ class Deque:
     def __init__(self):
         self.items = []
 
-    def isEmpty(self):
+    def is_empty(self):
         return self.items == []
 
-    def addFront(self, item):
+    def add_front(self, item):
         self.items.append(item)
 
-    def addRear(self, item):
+    def add_rear(self, item):
         self.items.insert(0,item)
 
-    def removeFront(self):
+    def remove_front(self):
         return self.items.pop()
 
-    def removeRear(self):
+    def remove_rear(self):
         return self.items.pop(0)
 
     def size(self):
