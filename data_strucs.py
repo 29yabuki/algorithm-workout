c23fa067ef69069ee9e9dc999554675139dc037f
29yabuki <skybluenako@gmail.com>
2024-06-03 22:31:56 +0800

    added whitespaces

--- data_strucs.py
+++ data_strucs.py
@@ -72,7 +72,7 @@ class HashTable:
 
     def put(self, key, data):
         hash_value = self.hash_func(key, len(self.slots))
-
+        
         if self.slots[hash_value] == None:
             self.slots[hash_value] = key
             self.data[hash_value] = data
@@ -81,10 +81,10 @@ class HashTable:
                 self.data[hash_value] = data
             else:
                 next_slot = self.rehash(hash_value, len(self.slots))
-            while self.slots[next_slot] != None and \
-                            self.slots[next_slot] != key:
+            
+            while self.slots[next_slot] != None and self.slots[next_slot] != key:
                 next_slot = self.rehash(next_slot, len(self.slots))
-
+            
             if self.slots[next_slot] == None:
                 self.slots[next_slot] = key
                 self.data[next_slot] = data
@@ -98,19 +98,20 @@ class HashTable:
         found = False
         position = start_slot
         
-        while self.slots[position] != None and  \
-                            not found and not stop:
+        while self.slots[position] != None and not found and not stop:
             if self.slots[position] == key:
                 found = True
                 data = self.data[position]
             else:
                 position = self.rehash(position, len(self.slots))
+            
             if position == start_slot:
                 stop = True
+        
         return data
 
     def __getitem__(self, key):
         return self.get(key)
 
     def __setitem__(self, key, data):
-        self.put(key,data)
+        self.put(key, data)
